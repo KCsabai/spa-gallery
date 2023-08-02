9cb4722d2dccb3de0fa71b924c5c824a0465cf83
CsabaK97 <kiss.csaba@minic.ro>
2023-08-02 21:37:32 +0300

    saga middleware added

--- src/app/rootSaga.jsx
+++ src/app/rootSaga.jsx
@@ -1,4 +1,5 @@
-import { all, call, put, takeLatest } from "redux-saga/effects";
+import { all, call, put, fork, take } from "redux-saga/effects";
+import { successAction, failedAction, pendingAction } from "../common/functions";
 
 const fetchRequest = ({url, method, data}) =>
   fetch(`${process.env.REACT_APP_BASE_API_URL}${url}`, {
@@ -6,40 +7,55 @@ const fetchRequest = ({url, method, data}) =>
     body: data,
   });
 
+const fetchPending = (data) => ({
+  type: pendingAction(data.type),
+})
 
 const fetchSuccess = (data) => ({
-   type: data.type,
-   data
+  type: successAction(data.type),
+  data
 });
   
 const fetchFailure = (data) => ({
-    type: data.FETCH_POST_FAILURE,
-    data
+  type: failedAction(data.type),
+  data
 });
   
 function* fetchSaga(action) {
-    console.log('action', action)
+  yield put(fetchPending(action));
   try {
     const response = yield call(fetchRequest, action);
     yield put(
-        fetchSuccess({
-            posts: response.data,
-            type: `${action.type}_SUCCESS`,
-        })
+      fetchSuccess({
+          data: response.data,
+          type: action.type,
+      })
     );
   } catch (e) {
     yield put(
       fetchFailure({
         error: e.message,
-        type: `${action.type}_FAILD`,
+        type: action.type,
       })
     );
   }
 }
 
+export const takeEveryRegex = (pattern, saga, ...args) =>
+  fork(function* () {
+    while (true) {
+      const action = yield take("*");
+      if (pattern.test(action.type)) {
+        yield fork(saga, ...args.concat(action))
+      }
+    }
+  });
+
 function* rootSaga() {
-  yield all([takeLatest('*_REQUEST', fetchSaga)]);
-}
+  // the api call needs to be begin with 'FETCH_' string and end without succesAction, failedAction, pendingAction string
+  const regexString = `^(?!(FETCH_[A-Z_]+(${successAction('')}|${failedAction('')}|${pendingAction('')})))FETCH_[A-Z_]+$`
+  yield all([takeEveryRegex(new RegExp(regexString), fetchSaga)]);
+};
 
 export default rootSaga;
 
